27494db21aa8ffa9490072f621698d490612832e
luladiniello6 <ldiniello@udesa.edu.ar>
2026-04-06 17:27:28 -0300

    agregue controlar productos

--- db/data.js
+++ db/data.js
@@ -1,5 +1,22 @@
 const data = {
-    productos: [],
+    productos: [
+        {
+            id: 1,
+            nombre: 'hola1',
+            genero: 'hombre',
+            descripcion: 'origen arabe',
+            capacidad: 500,
+            foto: 'images/'
+        }, 
+        {
+            id: 2,
+            nombre: 'hola2',
+            genero: 'mujer',
+            descripcion: 'origen chino',
+            capacidad: 500,
+            foto: 'images/'
+        }
+    ],
     usuario: {},
     comentarios: []
 }
--- routes/producto.js
+++ routes/producto.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const productController = require('../controllers/productController');
+
 router.get('/', productController.index);
 
 router.get('/')
