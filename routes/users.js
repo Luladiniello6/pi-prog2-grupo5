bc7f093da71ca02779afb9cafd2a7b7ec0c90dbf
luladiniello6 <ldiniello@udesa.edu.ar>
2026-06-08 16:05:28 -0300

    agregue controladores para procesar login y register

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,21 @@ var router = express.Router();
 var userController = require('../controllers/userController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-
+/* ruta por GET que envia el formulario de creacion */
 router.get('/login', userController.login);
+router.post('/login', userController.loginprocess)
+
 router.get('/register', userController.register);
+/* ruta por POST que procesa la informacion del formulario */
+router.post('/register', userController.registerprocess);
+
 router.get('/profile', userController.profile);
+router.post('/profile', userController.profileprocess)
+
 router.get('/logout', userController.logout);
+router.post('/logout', userController.logoutprocess)
 
 module.exports = router;
